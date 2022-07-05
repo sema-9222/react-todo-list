b6f59100fb51b24fc9647033985808cb96c56c56
sema-9222 <sema.cokoglu@hotmail.com>
2022-07-05 23:01:00 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Name } from "./components/Name";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi! My name is Sema!</p>
+        <Name name="Sema" feeling="Sleepy" />
+        <Name name="Radek" feeling="Sleepy too" />
       </header>
     </div>
   );
---
+++ src/components/Name.js
@@ -0,0 +1,9 @@
+import "./Name.css";
+export function Name(props) {
+  return (
+    <p className="name">
+      {" "}
+      I am {props.name} and I am {props.feeling}!
+    </p>
+  );
+}
