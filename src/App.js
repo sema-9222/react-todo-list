064cce97c11114b35f15ad2ae261655570067af7
sema-9222 <sema.cokoglu@hotmail.com>
2022-07-05 23:20:11 +0300

    button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Name } from "./components/Name";
+import { Button } from "./components/Button";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Name name="Sema" feeling="Sleepy" />
         <Name name="Radek" feeling="Sleepy too" />
+        <Button />
       </header>
     </div>
   );
--- src/components/Name.js
+++ src/components/Name.js
@@ -2,7 +2,6 @@ import "./Name.css";
 export function Name(props) {
   return (
     <p className="name">
-      {" "}
       I am {props.name} and I am {props.feeling}!
     </p>
   );
